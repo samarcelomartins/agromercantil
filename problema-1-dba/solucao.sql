93c0ffd4aabc53e6139d2613fad7b899934cf92a
samarcelomartins <147209384+samarcelomartins@users.noreply.github.com>
2025-01-13 23:11:53 -0300

    Update solucao.sql

--- problema-1-dba/solucao.sql
+++ problema-1-dba/solucao.sql
@@ -12,8 +12,8 @@ CREATE TABLE logs_20250101 PARTITION OF logs
 CREATE TABLE logs_20250108 PARTITION OF logs
     FOR VALUES FROM ('2025-01-08') TO ('2025-01-15');
 
--- Índice na coluna de tempo dos logs
-CREATE INDEX idx_logs_log_time ON logs (log_time);
+-- Índice BRIN na coluna de tempo dos logs
+CREATE INDEX idx_logs_log_time ON logs USING BRIN (log_time);
 
 -- Backup diário usando pg_dump
 pg_dump -U postgres -F c -b -v -f "/path/to/backup/backup_$(date +\%Y\%m\%d).dump" mydatabase
